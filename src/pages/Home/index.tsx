f8ba5669d21ff840970ad08d23337f5af3b3ec19
Beatriz Felix <mbeatrizmfelix@gmail.com>
2022-12-18 11:57:55 -0300

    feat: home component with timer

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,58 @@
+import { Play } from 'phosphor-react'
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">I am going to work in</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="your task"
+          />
+          <datalist id="task-suggestions">
+            <option value="task 1" />
+            <option value="task 2" />
+            <option value="task 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">during</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="25"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
